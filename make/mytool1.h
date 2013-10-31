147440f6fccc14ce258ff198853100a3f079734e
Mallius <mallius@qq.com>
2013-10-31 16:38:39 +0800

    make pro

---
+++ make/mytool1.h
@@ -0,0 +1,6 @@
+#ifndef __MYTOOL1_H__
+#define __MYTOOL1_H__
+
+void mytool1_print(char *print_str);
+
+#endif
---
+++ make/mytool2.h
@@ -0,0 +1,6 @@
+#ifndef __MYTOOL2_H__
+#define __MYTOOL2_H__
+
+void mytool2_print(char *print_str);
+
+#endif
